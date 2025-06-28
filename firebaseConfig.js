181dabdca052c88b6aebded0380e172ad3cd4bdb
utsavbhardwaj <utsavjha.me@gmail.com>
2025-06-28 18:13:40 +0530

    upload button fixed

--- firebaseConfig.js
+++ firebaseConfig.js
@@ -5,6 +5,9 @@ import { initializeApp } from "firebase/app";
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+console.log("Firebase API Key:", process.env.NEXT_PUBLIC_FIREBASE_API_KEY);
+console.log("Storage bucket:", process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET);
+
 const firebaseConfig = {
   apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
   authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,

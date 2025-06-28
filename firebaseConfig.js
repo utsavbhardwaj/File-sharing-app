3d368f727913486f1896d351db8229ce4044d7ae
utsavbhardwaj <utsavjha.me@gmail.com>
2025-06-28 16:58:52 +0530

    firebase config updated

--- firebaseConfig.js
+++ firebaseConfig.js
@@ -6,13 +6,13 @@ import { initializeApp } from "firebase/app";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyDQjigT7dr1i6M5O2W9D7SGNPRPpXLvdUc",
-  authDomain: "fileweb-8bf55.firebaseapp.com",
-  projectId: "fileweb-8bf55",
-  storageBucket: "fileweb-8bf55.appspot.com",
-  messagingSenderId: "279105519072",
-  appId: "1:279105519072:web:1a073b2105e4a4ed9aff85",
-  measurementId: "G-JYVQ96JM0D"
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID
 };
 
 // Initi  alize Firebase
